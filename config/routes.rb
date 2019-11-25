e9861c3df40a9205b44a327f6338ff0a896b979a
jeremie rousseau <jere.rousseau@gmail.com>
2019-11-25 17:44:51 +0100

    setted up devise. added a navbar available on all pages. TODO: change the links within the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :chickens, only:[:index, :show, :new, :create] do
-    resources :bookings
+  resources :chickens, only:[:index, :show] do
+    resources :bookings, only: [:create]
   end
 
-  resources :bookings, only:[:index, :show, :create]
+  resources :bookings, only:[:index]
 
   namespace :owner do
     resources :chickens, only:[:index, :new, :create]
-    resources :bookings, only:[:index]
+    resources :bookings, only:[:index] do
+      member do
+        patch "accept"
+        patch "decline"
+      end
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
