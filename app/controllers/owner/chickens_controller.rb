88da5a3cb2e005ca2a9024723239d16ee8db3806
Nicolas Guimier <nicolas.g@flexishore.com>
2019-11-26 17:38:01 +0100

    Create chicken form - Html ended. POST not working

--- app/controllers/owner/chickens_controller.rb
+++ app/controllers/owner/chickens_controller.rb
@@ -10,7 +10,18 @@ module Owner
     end
 
     def create
+      @chicken = Chicken.new(chicken_params)
+      if @chicken.save
+        redirect_to owner_chicken_path
+      else
+        render :new
+      end
+    end
 
+    private
+
+    def chicken_params
+      params.require(:chicken).permit(:name, :color, :species, :gender, :price, :birthdate, :photo, :presentation, :address, :owner)
     end
   end
 end
