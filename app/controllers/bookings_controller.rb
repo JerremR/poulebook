ab8cf87381b4d13eb7562972857e903180e7c8bc
Nicolas Guimier <nicolas.g@flexishore.com>
2019-11-27 14:37:22 +0100

    Create the post of new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @bookings = Booking.where(user: current_user)
+    @bookings = Booking.order(id: :desc).where(user: current_user)
   end
 
   def new
@@ -12,5 +12,26 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @chicken = Chicken.find(params[:chicken_id])
+    @booking.chicken = @chicken
+    @booking.user = current_user
+    @booking.total_price = calculate_total_price(@chicken.price, @booking.start_date, @booking.end_date)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def calculate_total_price(day_price, start_date, end_date)
+    total_price = day_price * (end_date - start_date)
+    total_price.round(2)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
