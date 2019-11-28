fd9313dd1f1fa703e7ecdfd3e69f026982d7c67c
jeremie rousseau <jere.rousseau@gmail.com>
2019-11-28 12:08:10 +0100

    updated search bar with SQL requests

--- app/controllers/chickens_controller.rb
+++ app/controllers/chickens_controller.rb
@@ -1,8 +1,16 @@
 class ChickensController < ApplicationController
   def index
-    @chickens = Chicken.geocoded
+    #Added the following for the search bar
+    if params[:query].present?
+      sql_query = "species ILIKE :query OR address ILIKE :query"
+      @chickens = Chicken.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @chickens = Chicken.all
+    end
 
-    @markers = @chickens.map do |chicken|
+    @geocoded_chickens = Chicken.geocoded
+
+    @markers = @geocoded_chickens.map do |chicken|
       {
         lat: chicken.latitude,
         lng: chicken.longitude,
