e70b27f5124bbc28504bb937a5122c6916e3a935
Nicolas Guimier <nicolas.g@flexishore.com>
2019-11-28 14:47:57 +0100

    Owner can update his booking

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -4,5 +4,22 @@ module Owner
     def index
       @bookings = current_user.owner_bookings
     end
+
+    def accept
+      change_status_to('Confirmé')
+    end
+
+    def decline
+      change_status_to('Refusé')
+    end
+
+    private
+
+    def change_status_to(string)
+      @booking = Booking.find(params[:id])
+      @booking.status = string
+      @booking.save
+      redirect_to owner_bookings_path
+    end
   end
 end
